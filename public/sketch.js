ee13e5e08ee1d43b398de93b7073cc0484e20c23
Rasheeda <rassalaam@gmail.com>
2021-06-26 02:59:30 +0200

    added some comments to sketch.js to plan next steps

--- public/sketch.js
+++ public/sketch.js
@@ -13,6 +13,9 @@ function gotPopsicle(data) {
   let drawing = data.drawing;
   console.log(drawing)
   
+  //Each path is 2 arrays (the x and y values for each drawing)
+  //For demo purposes, set the form field type as password, so no one can see what you're typing!!!
+  //You can do a simple onClick even to call gotPopsicle when submit is pressed, and take the value of the input as the first argument in server.js's fs call
   for(let path of drawing) {
     noFill();
     stroke(0)
