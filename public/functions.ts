612bd5ad0e3cad995076f7ff0717ba8c4d66cfaf
Rasheeda <rassalaam@gmail.com>
2021-06-27 20:54:29 +0200

    added timer-running-out mention in script.js

--- public/functions.ts
+++
@@ -1,7 +0,0 @@
-export const fileNames = ['anvil', 'apple', 'arm', 'asparagus', 'axe']
-
-const ndjsonFileName = () => {
-    return fileNames[Math.floor(Math.random() * fileNames.length)]
-}
-
-export default ndjsonFileName
